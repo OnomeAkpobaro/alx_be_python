a0067f9088f9960d7dc0574c2b57caf5fdcfc41a
Onome Akpobaro <overdoseofgodsblessings@gmail.com>
2024-09-13 11:12:01 +0100

    python_introduction/rectangle_area.py

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,9 @@
+#Define the variables
+length = 10           #Length of the triangle
+width = 5             #Width of the triangle
+
+#Calculate the Area
+Area = length * width
+
+#Print the Area
+print(f"The area of the rectangle is: {Area}")
