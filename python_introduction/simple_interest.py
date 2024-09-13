a4009999478e2595466e3e32bae65d9bda07eb18
Onome Akpobaro <overdoseofgodsblessings@gmail.com>
2024-09-13 11:03:58 +0100

    python_introduction/simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+#Define the variable
+principal = 1000  #principal in dollars
+rate = 0.05       #Annual interest rate (5%)
+time = 3          #Time period in years
+
+#Calculate the simple interest
+interest = principal * rate * time
+
+#Print the calculated interest
+print(f"The simple interest is: {interest}")
