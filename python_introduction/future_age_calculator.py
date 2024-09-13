8b520a613ab74de4108aa683dd085de9c0f46200
Onome Akpobaro <overdoseofgodsblessings@gmail.com>
2024-09-13 11:49:38 +0100

    python_introduction/future_age_calculator.py

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,9 @@
+# Prompt the user to input thier current age
+current_age = int(input("How old are you"))
+
+# Calculate the user's age in the year 2050
+years_to_add = 2050 -2023
+future_age = current_age + years_to_add
+
+# Print the result in the require format
+print(f"In 2050, you will be {future_age} years old")
