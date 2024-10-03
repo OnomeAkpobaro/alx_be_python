4555360bf816ec0e0f8542000a59332c4845ba87
Onome Akpobaro <overdoseofgodsblessings@gmail.com>
2024-10-03 21:50:10 +0100

    Added new files

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,16 @@
+
+def safe_divide(numerator, denominator):
+    
+    try:
+        
+        num = float(numerator)
+        dnom = float(denominator)
+        result = num / dnom
+        return f"The result of the division is {result}"
+    
+    except ZeroDivisionError:
+        return "Error: Cannot divide by zero."
+
+    except ValueError:
+        return "Error: please enter numeric values only."
+
