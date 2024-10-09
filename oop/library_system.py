d7ab25ef72052c21d52c4c6b041813a1071cef38
Onome Akpobaro <overdoseofgodsblessings@gmail.com>
2024-10-09 16:47:11 +0100

    Updated files

--- oop/library_system.py
+++ oop/library_system.py
@@ -25,7 +25,7 @@ class PrintBook(Book):
         self.page_count = page_count
     def __str__(self):
         """String representation of PrintBook."""
-        return f"PrintBook: {self.title} by {self.author}, Page count: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
     
 class Library:
     def __init__(self):
