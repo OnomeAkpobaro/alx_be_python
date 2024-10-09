01b1b70cdfba2a9391ca6fd13c9552023d2a76dd
Onome Akpobaro <overdoseofgodsblessings@gmail.com>
2024-10-09 16:33:27 +0100

    Added new file and updated main.py to 0main.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,14 +1,16 @@
 class Book:
     def __init__(self, title, author, year):
-        "Initializes a Book injstance with title, author and year "
+        """Initializes a Book injstance with title, author and year"""
         self.title = title
         self.author = author
         self.year = year
     def __del__(self):
-        "Destructor that is called when an object is deleted."
+        """Destructor that is called when an object is deleted."""
         print(f"Deleting {self.title}")
     def __str__(self):
+        """Returns a string representation of the Book"""
         return f"{self.title} by {self.author}, published in {self.year}"
     
     def __repr__(self):
+        """Returns an official string represntaion of the Book"""
         return f"Book('{self.title}', '{self.author}', {self.year})"
---
+++ oop/library_system.py
@@ -0,0 +1,51 @@
+class Book:
+    def __init__(self, title, author):
+        """Initializes Book instance"""
+
+        self.title = title
+        self.author = author
+    def __str__(self):
+        """String representation of the Book"""
+
+        return f"Book: {self.title} by {self.author}"
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        """Initializes EBook with additional file_size attribute."""
+        
+        self.file_size = file_size
+    def __str__(self):
+        """String representation of the EBook."""
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        """Initializes PrintBook with additional page_count attribute."""
+
+        self.page_count = page_count
+    def __str__(self):
+        """String representation of PrintBook."""
+        return f"PrintBook: {self.title} by {self.author}, Page count: {self.page_count}"
+    
+class Library:
+    def __init__(self):
+        """Initializes an empty Library"""
+
+        self.books = []
+    
+    def add_book(self, book):
+        """Add a Book, EBook or PrintBook instance to the Library."""
+        self.books.append(book)
+
+    def list_books(self):
+        """Print details of book in the Library."""
+        for book in self.books:
+            print(book)
+
+
+
+   
+        
+        
+        
+
